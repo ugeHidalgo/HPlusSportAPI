e618400f22a564a14e2000dd47ca50dc6bbc9c65
Eugenio Hidalgo Hernández <eugenio.hidalgo@unit4.com>
2022-11-08 13:41:11 +0100

    Initial commit

---
+++ HPlusSport.API/Models/ShopContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HPlusSport.API.Models
+{
+    public class ShopContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasMany(p => p.Products).WithOne(c => c.Category);
+        }
+    }
+}
